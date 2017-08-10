2899d45425b1afd911ce66adc1062bff42afd8de
andres.gomez <andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.com>
2017-08-10 13:47:25 -0700

    Added DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -32,7 +32,6 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET /todos/123
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -46,8 +45,21 @@ app.get('/todos/:id', (req, res) => {
     }
     res.send({todo});
   }).catch((e) => res.status(400).send());
+});
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
